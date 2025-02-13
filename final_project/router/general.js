70c327aa0111b0a7afed55cfc6c7ba4c098c2932
Alexander Cardoselli <138590454+batmanwh0laughs@users.noreply.github.com>
2025-02-13 16:15:47 +0100

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -28,19 +28,9 @@ const getBooks = () => {
     });
 };
 
-/*
+
 //  Task 1
 //  Get the book list available in the shop
-public_users.get('/',async function (req, res) {
-  try {
-    const bookList = await getBooks(); 
-    res.json(bookList); // Neatly format JSON output
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Error retrieving book list" });
-  }
-});
-*/
 public_users.get('/', async function (req, res) {
     try {
       const bookList = await getBooks(); 
@@ -78,15 +68,7 @@ public_users.get('/isbn/:isbn',function (req, res) {
 
 //  Task 3 & Task 12
 //  Get book details based on author
-/*
-public_users.get('/author/:author',function (req, res) {
-    const author = req.params.author;
-    getBooks()
-    .then((bookEntries) => Object.values(bookEntries))
-    .then((books) => books.filter((book) => book.author === author))
-    .then((filteredBooks) => res.send(filteredBooks));
-});
-*/
+
 public_users.get('/author/:author', function (req, res) {
     const author = req.params.author;
     getBooks()
@@ -105,15 +87,7 @@ public_users.get('/author/:author', function (req, res) {
 
 //  Task 4 & Task 12
 //  Get all books based on title
-/*
-public_users.get('/title/:title',function (req, res) {
-    const title = req.params.title;
-    getBooks()
-    .then((bookEntries) => Object.values(bookEntries))
-    .then((books) => books.filter((book) => book.title === title))
-    .then((filteredBooks) => res.send(filteredBooks));
-});
-*/
+
 public_users.get('/title/:title', function (req, res) {
     const title = req.params.title;
     
